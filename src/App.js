8e1ae7d48502b713bb7e3c0e719e0d8bd5d3d0fa
Mitzi17 <m_estrada17@live.com>
2023-08-02 11:18:37 -0700

    removed functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { myItems } from './Components/ItemsData';
+import Home from './Components/Home';
+import Items from './Components/Items';
+import ItemForm from './Components/ItemForm';
+
 import { 
   BrowserRouter as Router,
   Switch,
@@ -8,116 +13,46 @@ import {
 } from 'react-router-dom';
 
 
-export default function App() {
+export default class App extends React.Component {
 
-  const myItems = [
-    {
-      id: 1,
-      name: 'Harry Potter Book 5',
-      price: 10.99,
-      condition: 'used',
-      location: 'Los Angeles'
-  },
-  {
-    id: 2,
-    name: 'snowman ornament',
-    price: 7.99,
-    condition: 'used',
-    location: 'Denver'
-  },
-  {
-    id: 3,
-    name: 'nike backpack',
-    price: 15.99,
-    condition: 'new',
-    location: 'San Diego'
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: myItems || []
+    }
   }
 
-    ]
-  return (
-    < Router>
-      <div>
-        <ul>
-          <li>
-              <Link to="/">HOME</Link>
-          </li>
-          <li>
-              <Link to="/items">MY ITEMS</Link>
-          </li>
-          <li>
-              <Link to="/itemform">ADD NEW ITEM</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route path="/items">
-            <Items items={myItems}/>
-          </Route>
-          <Route path="/itemform">
-            <Form/>
-          </Route>
-          <Route path="/">
-            <Home/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return <h1>Home</h1>
-}
-
-function Items({ items }) {
-  const match = useRouteMatch();
-  const findItemById = (id) =>
-    items.filter((item) => item.id == id)[0];
+  render() {
   
-  return (
-    <div>
-      <h1>My Items</h1>
-      <ul>
-        {items.map((item, index) => {
-          return (
-            <li key={index}>
-              <Link to={`${match.url}/${item.id}`}>
-                {item.name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      <Switch>
-        <Route
-          path={`${match.path}/:itemId`}
-          render={(props) => (
-            <Item
-              {...props}
-              data={findItemById(props.match.params.itemId)}
-            />
-          )}
-        />
-        <Route path={match.path}>
-            <h3>Select an item</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
-}
+      return (
 
-function Item(props) {
-  const { data } = props;
-  return (
-    <div>
-      <h3>{data.name}</h3>
-      <h4>Price: {data.price}</h4>
-      <h4>Condition: {data.condition}</h4>
-      <h4>Location: {data.location}</h4>
-    </div>
-  )
-}
-
-function Form() {
-  return <h1>New Item Form</h1>
+        < Router>
+          <div>
+           <ul>
+              <li>
+                <Link to="/">HOME</Link>
+              </li>
+              <li>
+                <Link to="/items">MY ITEMS</Link>
+              </li>
+              <li>
+                <Link to="/itemform">ADD NEW ITEM</Link>
+              </li>
+            </ul>
+            <Switch>
+              <Route path="/items">
+                <Items items={this.state.items}/>
+              </Route>
+              <Route path="/itemform">
+                <ItemForm/>
+              </Route>
+              <Route path="/">
+                <Home/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      );
+  } 
 }
 
