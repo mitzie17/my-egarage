c6486083d53911e408942f0090e1c34f3f985d5a
Mitzi17 <m_estrada17@live.com>
2023-08-02 17:07:16 -0700

    removed routes

--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { 
     BrowserRouter as Router,
     Switch,
@@ -6,17 +7,22 @@ import {
     useRouteMatch
   } from 'react-router-dom';
   import Item from './Item';
+  import { NewReviewForm } from './NewReviewForm';
+  import { itemsApi } from '../api/ItemsApi';
 
-export default function Items({items}) {
+function Items(props){
+console.log('items component')
+console.log(props)
+    const { items } = props;
     const match = useRouteMatch();
     const findItemById = (id) =>
-      items.filter((item) => item.id == id)[0];
-    
-    return (
-      <div>
-        <h1>My Items</h1>
-        <ul>
-          {items.map((item, index) => {
+        items.filter((item) => item.id == id)[0];
+
+        return(
+            <div>
+         <h1>My Items</h1>
+         <ul>
+           {items.map((item, index) => {
             return (
               <li key={index}>
                 <Link to={`${match.url}/${item.id}`}>
@@ -27,19 +33,20 @@ export default function Items({items}) {
           })}
         </ul>
         <Switch>
-          <Route
-            path={`${match.path}/:itemId`}
-            render={(props) => (
-              <Item
-                {...props}
-                data={findItemById(props.match.params.itemId)}
-              />
-            )}
-          />
-          <Route path={match.path}>
-              <h3>Select an item</h3>
-          </Route>
+            <Route
+                path={`${match.path}/:itemId`}
+                render={(props) => (
+                    <Item 
+                    {...props} item={findItemById(props.match.params.itemId)} updatesItem={props.updatesItem}
+                    />
+                )}
+            />
         </Switch>
+       
       </div>
-    );
+        )
+    
+    
   }
+
+  export default Items;
