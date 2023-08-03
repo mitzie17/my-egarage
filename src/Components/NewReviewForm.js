34649c115d9fb27a44251451b7b65c9ff98c6d73
Mitzi17 <m_estrada17@live.com>
2023-08-02 17:07:57 -0700

    added review form file

---
+++ src/Components/NewReviewForm.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+export const NewReviewForm = (props) => {
+    const [review, setReview] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (review) {
+            props.addNewReview(review)
+        } else {
+            console.log('invalid input')
+        }
+    };
+
+    return (
+        <div>
+            <h3>Add new review</h3>
+            <form onSubmit={onSubmit}>
+                <input type='text' placeholder='type your review' value='review' onChange={(e) => setReview(e.target.value)}/>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+
+}
