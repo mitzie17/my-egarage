80019e604ad04d9f6bcfd6f1cfebbc1e091b9ec5
Mitzi17 <m_estrada17@live.com>
2023-08-05 11:43:36 -0700

    imported bootstrap components

--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert';
 import { 
     BrowserRouter as Router,
     Switch,
@@ -11,8 +13,8 @@ import {
   import { itemsApi } from '../api/ItemsApi';
 
 function Items(props){
-console.log('items component')
-console.log(props)
+//console.log('items component')
+//console.log(props)
     const { items, updateItem, deleteItem } = props;
     const match = useRouteMatch();
     const findItemById = (id) =>
@@ -25,18 +27,18 @@ console.log(props)
         return(
             <div>
          <h1>My Items</h1>
-         <ul>
+         
            {items.map((item, index) => {
             return (
-              <li key={index}>
+              <Alert key={index} variant="primary">
                 <Link to={`${match.url}/${item.id}`}>
                   {item.name}
                 </Link>
                 <button onClick={(e) => removeItem(item.id)}>Delete</button>
-              </li>
+              </Alert>
             );
           })}
-        </ul>
+        
         <Switch>
             <Route
                 path={`${match.path}/:itemId`}
