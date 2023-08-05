94e7eb75da88b07447e988620e83d9eb33cd0c4c
Mitzi17 <m_estrada17@live.com>
2023-08-05 13:34:18 -0700

    changes in spacing

--- src/api/ItemsApi.js
+++ src/api/ItemsApi.js
@@ -1,63 +1,61 @@
-const ITEMS_API = 'https://64caa1a2700d50e3c705235a.mockapi.io/items';
+const ITEMS_API = "https://64caa1a2700d50e3c705235a.mockapi.io/items";
 
 class ItemsApi {
-    get = async () => {
-        try {
-            const resp = await fetch(ITEMS_API);
-            const data = await resp.json();
-            return data;
-        } catch(e) {
-            console.log('Oh no! Something went wrong in fetchItems.', e);
-        }
+  get = async () => {
+    try {
+      const resp = await fetch(ITEMS_API);
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.log("Oh no! Something went wrong in fetchItems.", e);
     }
+  };
 
-    put = async (item) => {
-        try {
-            const resp = await fetch(`${ITEMS_API}/${item.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(item)
-            });
-            return await resp.json();
-        } catch(e) {
-            console.log('Oh no! Looks like the item couldnt be updated.', e);
-        }
+  put = async (item) => {
+    try {
+      const resp = await fetch(`${ITEMS_API}/${item.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(item),
+      });
+      return await resp.json();
+    } catch (e) {
+      console.log("Oh no! Looks like the item couldnt be updated.", e);
     }
+  };
 
-    post = async (item) => {
-        try {
-            const resp = await fetch(`${ITEMS_API}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    'name': item.name,
-                    'price': item.price,
-                    'reviews': [...item.reviews, item.review]
-                })
-            });
-            const data = await response.json();
-            console.log(data)
-        } catch(e) {
-            console.log('Oh no! Item counldnt be created.')
-        }
+  post = async (item) => {
+    try {
+      const resp = await fetch(`${ITEMS_API}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: item.name,
+          price: item.price,
+          reviews: [...item.reviews, item.review],
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.log("Oh no! Item counldnt be created.");
     }
+  };
 
-    delete = async (itemId) => {
-        try {
-            const resp = await fetch(`${ITEMS_API}/${itemId}`, {
-                method: 'DELETE',
-            })
-            const data = await response.json();
-        } catch(e) {
-            console.log('Oh no! Item was not deleteed.')
-        }
+  delete = async (itemId) => {
+    try {
+      const resp = await fetch(`${ITEMS_API}/${itemId}`, {
+        method: "DELETE",
+      });
+      const data = await response.json();
+    } catch (e) {
+      console.log("Oh no! Item was not deleteed.");
     }
-    
+  };
 }
 
 export const itemsApi = new ItemsApi();
-
