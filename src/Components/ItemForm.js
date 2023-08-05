6c60cb0c0f6069f9c30bf06bb8b82660457efc75
Mitzi17 <m_estrada17@live.com>
2023-08-04 21:14:00 -0700

    added form to create item and defined onSubmit

--- src/Components/ItemForm.js
+++ src/Components/ItemForm.js
@@ -1,3 +1,36 @@
-export default function ItemForm() {
-    return <h1>New Item Form</h1>
-  }
+import React, { useState } from 'react';
+
+export const ItemForm = (props) => {
+    
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(name);
+        console.log(price);
+        const newItem = {
+            'name': name,
+            'price': price
+        }
+        props.createItem(newItem)
+        // if (item) {
+        //     props.addNewItem(item)
+        //     setItem('');
+        // } else {
+        //     console.log('invalid input')
+        // }
+    };
+
+    return (
+        <div>
+            <h3>Add new item</h3>
+            <form onSubmit={onSubmit}>
+                <input type='text' placeholder='item name'  onChange={(e) => setName(e.target.value)}/>
+                <input type='number' placeholder='price'  onChange={(e) => setPrice(e.target.value)}/>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+
+}
